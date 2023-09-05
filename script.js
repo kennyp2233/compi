eb78067877495e47d58b70f6502bc10c0336f44c
kenny pinchao <kennyp41234@gmail.com>
2023-09-05 02:13:31 -0500

    a

--- script.js
+++ script.js
@@ -1,97 +1,30 @@
 
-let hideText_btn = document.getElementById('hideText_btn');
-let hideText = document.getElementById('hideText');
-hideText_btn.addEventListener('click', toogleText);
+// Función para mostrar u ocultar contenido adicional
+function toggleContent(buttonId, contentId) {
+    var button = document.getElementById(buttonId);
+    var content = document.getElementById(contentId);
 
-function toogleText() {
-    hideText.classList.toggle('show');
-    if (hideText.classList.contains('show')) {
-        hideText_btn.innerHTML = 'Leer menos';
-    } else {
-        hideText_btn.innerHTML = 'Leer más';
+    if (button && content) {
+        if (content.style.display === "none" || content.style.display === "") {
+            content.style.display = "block";
+            button.textContent = "Cerrar";
+        } else {
+            content.style.display = "none";
+            button.textContent = "Leer más";
+        }
     }
 }
 
+// Asocia la función a los botones correspondientes
+document.getElementById("toggle-content-btn1").addEventListener("click", function () {
+    toggleContent("toggle-content-btn1", "hidden-content1");
+});
 
-let hideText_btn1 = document.getElementById('hideText_btn1');
-let hideText1 = document.getElementById('hideText1');
-hideText_btn1.addEventListener('click', toogleText1);
+document.getElementById("toggle-content-btn2").addEventListener("click", function () {
+    toggleContent("toggle-content-btn2", "hidden-content2");
+});
 
-function toogleText1() {
-    hideText1.classList.toggle('show1');
-    if (hideText1.classList.contains('show1')) {
-        hideText_btn1.innerHTML = 'Leer menos';
-    } else {
-        hideText_btn1.innerHTML = 'Leer más';
-    }
-}
+document.getElementById("toggle-content-btn3").addEventListener("click", function () {
+    toggleContent("toggle-content-btn3", "hidden-content3");
+});
 
-let hideText_btn3 = document.getElementById('hideText_btn3');
-let hideText3 = document.getElementById('hideText3');
-hideText_btn3.addEventListener('click', toogleText3);
-
-function toogleText3() {
-    console.log("asdasda")
-    hideText3.classList.toggle('show3');
-    if (hideText3.classList.contains('show3')) {
-        hideText_btn3.innerHTML = 'Leer menos';
-    } else {
-        hideText_btn3.innerHTML = 'Leer más';
-    }
-}
-
-
-let hideText_btn4 = document.getElementById('hideText_btn4');
-let hideText4 = document.getElementById('hideText4');
-hideText_btn4.addEventListener('click', toogleText4);
-
-function toogleText4() {
-    hideText4.classList.toggle('show4');
-    if (hideText4.classList.contains('show4')) {
-        hideText_btn4.innerHTML = 'Leer menos';
-    } else {
-        hideText_btn4.innerHTML = 'Leer más';
-    }
-}
-
-
-let hideText_btn5 = document.getElementById('hideText_btn5');
-let hideText5 = document.getElementById('hideText5');
-hideText_btn5.addEventListener('click', toogleText5);
-
-function toogleText5() {
-    hideText5.classList.toggle('show5');
-    if (hideText5.classList.contains('show5')) {
-        hideText_btn5.innerHTML = 'Leer menos';
-    } else {
-        hideText_btn5.innerHTML = 'Leer más';
-    }
-}
-
-
-let hideText_btn6 = document.getElementById('hideText_btn6');
-let hideText6 = document.getElementById('hideText6');
-hideText_btn6.addEventListener('click', toogleText6);
-
-function toogleText6() {
-    hideText6.classList.toggle('show6');
-    if (hideText6.classList.contains('show6')) {
-        hideText_btn6.innerHTML = 'Leer menos';
-    } else {
-        hideText_btn6.innerHTML = 'Leer más';
-    }
-}
-
-
-let hideText_btn7 = document.getElementById('hideText_btn7');
-let hideText7 = document.getElementById('hideText7');
-hideText_btn7.addEventListener('click', toogleText7);
-
-function toogleText7() {
-    hideText7.classList.toggle('show7');
-    if (hideText7.classList.contains('show7')) {
-        hideText_btn7.innerHTML = 'Leer menos';
-    } else {
-        hideText_btn7.innerHTML = 'Leer más';
-    }
-}
