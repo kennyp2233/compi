cd6a229145f495c3a6df467346459a09efd4a800
kenny pinchao <kennyp41234@gmail.com>
2023-09-05 00:09:40 -0500

    mierda

---
+++ script.js
@@ -0,0 +1,112 @@
+
+let hideText_btn = document.getElementById('hideText_btn');
+let hideText = document.getElementById('hideText');
+hideText_btn.addEventListener('click', toogleText);
+
+function toogleText() {
+    hideText.classList.toggle('show');
+    if (hideText.classList.contains('show')) {
+        hideText_btn.innerHTML = 'Leer menos';
+    } else {
+        hideText_btn.innerHTML = 'Leer más';
+    }
+}
+
+
+let hideText_btn1 = document.getElementById('hideText_btn1');
+let hideText1 = document.getElementById('hideText1');
+hideText_btn1.addEventListener('click', toogleText1);
+
+function toogleText1() {
+    hideText1.classList.toggle('show1');
+    if (hideText1.classList.contains('show1')) {
+        hideText_btn1.innerHTML = 'Leer menos';
+    } else {
+        hideText_btn1.innerHTML = 'Leer más';
+    }
+}
+
+
+let hideText_btn2 = document.getElementById('hideText_btn2');
+let hideText2 = document.getElementById('hideText2');
+hideText_btn2.addEventListener('click', toogleText2);
+
+function toogleText2() {
+    hideText2.classList.toggle('show2');
+    if (hideText2.classList.contains('show2')) {
+        hideText_btn2.innerHTML = 'Leer menos';
+    } else {
+        hideText_btn2.innerHTML = 'Leer más';
+    }
+}
+
+
+
+let hideText_btn3 = document.getElementById('hideText_btn3');
+let hideText3 = document.getElementById('hideText3');
+hideText_btn3.addEventListener('click', toogleText3);
+
+function toogleText3() {
+    hideText3.classList.toggle('show3');
+    if (hideText3.classList.contains('show3')) {
+        hideText_btn3.innerHTML = 'Leer menos';
+    } else {
+        hideText_btn3.innerHTML = 'Leer más';
+    }
+}
+
+
+let hideText_btn4 = document.getElementById('hideText_btn4');
+let hideText4 = document.getElementById('hideText4');
+hideText_btn4.addEventListener('click', toogleText4);
+
+function toogleText4() {
+    hideText4.classList.toggle('show4');
+    if (hideText4.classList.contains('show4')) {
+        hideText_btn4.innerHTML = 'Leer menos';
+    } else {
+        hideText_btn4.innerHTML = 'Leer más';
+    }
+}
+
+
+let hideText_btn5 = document.getElementById('hideText_btn5');
+let hideText5 = document.getElementById('hideText5');
+hideText_btn5.addEventListener('click', toogleText5);
+
+function toogleText5() {
+    hideText5.classList.toggle('show5');
+    if (hideText5.classList.contains('show5')) {
+        hideText_btn5.innerHTML = 'Leer menos';
+    } else {
+        hideText_btn5.innerHTML = 'Leer más';
+    }
+}
+
+
+let hideText_btn6 = document.getElementById('hideText_btn6');
+let hideText6 = document.getElementById('hideText6');
+hideText_btn6.addEventListener('click', toogleText6);
+
+function toogleText6() {
+    hideText6.classList.toggle('show6');
+    if (hideText6.classList.contains('show6')) {
+        hideText_btn6.innerHTML = 'Leer menos';
+    } else {
+        hideText_btn6.innerHTML = 'Leer más';
+    }
+}
+
+
+let hideText_btn7 = document.getElementById('hideText_btn7');
+let hideText7 = document.getElementById('hideText7');
+hideText_btn7.addEventListener('click', toogleText7);
+
+function toogleText7() {
+    hideText7.classList.toggle('show7');
+    if (hideText7.classList.contains('show7')) {
+        hideText_btn7.innerHTML = 'Leer menos';
+    } else {
+        hideText_btn7.innerHTML = 'Leer más';
+    }
+}
